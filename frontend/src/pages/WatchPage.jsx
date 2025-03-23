3a4ae054c2d244dd426dfc761a0386c583107e92
sabana <messyme154@gmail.com>
2025-03-23 21:54:29 +0530

    de

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -6,7 +6,8 @@ import Navbar from "../components/Navbar";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import ReactPlayer from "react-player";
 import { ORIGINAL_IMG_BASE_URL, SMALL_IMG_BASE_URL } from "../utils/constants.js";
-
+import { formatReleaseDate } from "../utils/dateFunction";
+import WatchPageSkeleton from "../components/skeletons/WatchPageSkeleton";
 
 const WatchPage = () => {
 	const { id } = useParams();
